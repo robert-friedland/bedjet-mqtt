39b9bbc8fedfa7158731f6fed7108e3ca3d0f587
Robert Friedland <robertfriedland1@gmail.com>
2022-10-22 19:48:22 -0400

    scan

--- scan.py
+++ scan.py
@@ -2,7 +2,7 @@ import asyncio
 from bleak import BleakScanner
 import logging
 
-logging.getLogger().setLevel(logging.DEBUG)
+logging.getLogger().setLevel(logging.INFO)
 
 
 async def main():
@@ -12,16 +12,14 @@ async def main():
 
     def callback(device, advertising_data):
         # TODO: do something with incoming data
-        logging.debug(device)
-        logging.debug(advertising_data)
+        logging.info(device)
+        logging.info(advertising_data)
 
     async with BleakScanner(callback) as scanner:
-        ...
         # Important! Wait for an event to trigger stop, otherwise scanner
         # will stop immediately.
         await stop_event.wait()
 
     # scanner stops when block exits
-    ...
 
 asyncio.run(main())
