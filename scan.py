2c24937cce5510716a7a460e15e6a5adc47936ca
Robert Friedland <robertfriedland1@gmail.com>
2022-10-22 19:51:15 -0400

    scan

--- scan.py
+++ scan.py
@@ -6,20 +6,7 @@ logging.getLogger().setLevel(logging.INFO)
 
 
 async def main():
-    stop_event = asyncio.Event()
-
-    # TODO: add something that calls stop_event.set()
-
-    def callback(device, advertising_data):
-        # TODO: do something with incoming data
-        logging.info(device)
-        logging.info(advertising_data)
-
-    async with BleakScanner(callback) as scanner:
-        # Important! Wait for an event to trigger stop, otherwise scanner
-        # will stop immediately.
-        await stop_event.wait()
-
-    # scanner stops when block exits
+    devices = await BleakScanner.discover()
+    logging.info(devices)
 
 asyncio.run(main())
